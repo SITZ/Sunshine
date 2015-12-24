6dddba8ff53254db57adaf296223cdc395886921
Sitesh Shrivastava <siteshshrivastava@gmail.com>
2015-12-25 05:15:47 +0530

    Sunshine: Lecture 2

--- app/src/main/java/com/siteshshrivastava/sunshine/MainActivity.java
+++ app/src/main/java/com/siteshshrivastava/sunshine/MainActivity.java
@@ -1,11 +1,8 @@
 package com.siteshshrivastava.sunshine;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -31,11 +28,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // as you specify a parent activity in AndroidManifest.xml
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_settings) {
             return true;
         }
 
